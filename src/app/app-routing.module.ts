98fa7376237236edec7099f9c9cb6ef7d85d30a2
benzarty <m.benzarti.1996@gmail.com>
2019-11-20 02:26:23 +0100

    8odwa manich jey

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,15 @@ import {RouterModule, Route, Routes} from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {LoginComponent} from './login/login.component';
 import {FormEventComponent} from './dashboard/Events/form-event/form-event.component';
+import {MatNavComponent} from './mat-nav/mat-nav.component';
+import {MatTableComponent} from './mat-table/mat-table.component';
 
 
 const routes: Routes = [
   {path: '', component: LoginComponent
-  }, {path : 'dash-board', component: DashboardComponent},
-  {path : 'lol', component: FormEventComponent},
+  }, {path: 'lol', component: MatNavComponent
+  },
+
   {
     path: 'dashboard', component: DashboardComponent, children: [{
       path: 'formulaire', component: FormEventComponent}] }];
